09e758f167f8109eab5be76e969be8a6706b9b9b
atulpat3l <patel.ap787@gmail.com>
2022-05-22 18:01:38 +0530

    added functionality that allow only note creator to delete that note

--- notes-backend/controllers/notes.js
+++ notes-backend/controllers/notes.js
@@ -1,24 +1,25 @@
-const notesRouter = require("express").Router();
-const jwt = require("jsonwebtoken");
+const notesRouter = require('express').Router();
+const { decode } = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-const Note = require("../models/note");
-const User = require("../models/user");
+const Note = require('../models/note');
+const User = require('../models/user');
 
 const getTokenFrom = (request) => {
-  const authorization = request.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
     return authorization.substring(7);
   }
   return null;
 };
 
-notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 });
 
   response.json(notes);
 });
 
-notesRouter.get("/:id", async (request, response) => {
+notesRouter.get('/:id', async (request, response) => {
   const note = await Note.findById(request.params.id);
 
   if (note) {
@@ -28,13 +29,13 @@ notesRouter.get("/:id", async (request, response) => {
   }
 });
 
-notesRouter.post("/", async (request, response) => {
+notesRouter.post('/', async (request, response) => {
   const { content, important } = request.body;
 
   const token = getTokenFrom(request);
   const decodedToken = jwt.verify(token, process.env.SECRET);
   if (!token || !decodedToken.id) {
-    return response.status(401).json({ error: "token missing or invalid" });
+    return response.status(401).json({ error: 'token missing or invalid' });
   }
   const user = await User.findById(decodedToken.id);
 
@@ -52,12 +53,30 @@ notesRouter.post("/", async (request, response) => {
   response.status(201).json(savedNote);
 });
 
-notesRouter.delete("/:id", async (request, response) => {
-  await Note.findByIdAndRemove(request.params.id);
-  response.status(204).end();
+notesRouter.delete('/:id', async (request, response) => {
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+
+  if (!decodedToken.id) {
+    return response.status(401).json({
+      error: 'token missing or invalid',
+    });
+  }
+
+  const userId = decodedToken.id.toString();
+
+  const blogToDelete = await Note.findById(request.params.id);
+  const blogId = blogToDelete.user.toString();
+
+  if (userId === blogId) {
+    await blogToDelete.remove();
+    response.status(204).end();
+  } else {
+    response.status(401).end();
+  }
 });
 
-notesRouter.put("/:id", (request, response, next) => {
+notesRouter.put('/:id', (request, response, next) => {
   const body = request.body;
 
   const note = {
